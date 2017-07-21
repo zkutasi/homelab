430b7a9e99d52bdb0340a751bb2ff5241d6b0198
zkutasi <z.kutasi@gmail.com>
2017-07-21 20:40:09 +0200

    Fixing missing pushover information in push_torrent.sh

--- push_torrent.sh
+++ push_torrent.sh
@@ -3,11 +3,16 @@
 # This script can push a message whenever a torrent is downloaded, and also the free disk space available
 # Requirements:
 #    - A Torrent client that supports such functionality (like rTorrent)
+#    - Create a file pushover.info, with the following format:
+#      token=YOURAPITOKEN
+#      user=YOURUSERTOKEN
 #    - Edit .rtorrent.rc and add the following line at the end
 #      method.set_key = event.download.finished,notify_me,"execute=~/push_torrent.sh,$d.name="
 #    - Fill in the quota you have
 # Arguments:
 #    - The torrent name (will come from the client)
+
+source pushover.info
 
 name=$1
 QUOTA=1500
