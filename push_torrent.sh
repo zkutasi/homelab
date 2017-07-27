b8f57515040d8dc9b3a41cd64336e772a1601a20
Zoltan Kutasi <z.kutasi@gmail.com>
2017-07-28 00:17:35 +0200

    Some cosmetic improvements

--- push_torrent.sh
+++ push_torrent.sh
@@ -3,16 +3,15 @@
 # This script can push a message whenever a torrent is downloaded, and also the free disk space available
 # Requirements:
 #    - A Torrent client that supports such functionality (like rTorrent)
-#    - Create a file pushover.info, with the following format:
-#      token=YOURAPITOKEN
-#      user=YOURUSERTOKEN
 #    - Edit .rtorrent.rc and add the following line at the end
 #      method.set_key = event.download.finished,notify_me,"execute=~/push_torrent.sh,$d.name="
 #    - Fill in the quota you have
 # Arguments:
 #    - The torrent name (will come from the client)
 
-source pushover.info
+PUSH_SCRIPT="/home/send_push.sh"
+
+
 
 name=$1
 QUOTA=1500
@@ -23,7 +22,4 @@ occ_space=$(du ${HOME} --si -sh -BG 2>/dev/null | awk '{print $1}' | tr -d -c 0-
 free_space=$((QUOTA-occ_space))
 message2="Free space: ${free_space}GB / ${QUOTA}GB"
 
-curl -s --form-string "token=${token}" \
-        --form-string "user=${user}" \
-        --form-string "message=${message1}
-${message2}" https://api.pushover.net/1/messages.json
+${PUSH_SCRIPT} "${message}"
