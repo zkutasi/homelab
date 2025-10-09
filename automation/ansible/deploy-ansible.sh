fe1cdd31249851451223ea4e4be7a856a6d43e64
Zoltan Kutasi <z.kutasi@gmail.com>
2025-10-09 11:17:26 +0200

    Install ansible-lint too

--- automation/ansible/deploy-ansible.sh
+++ automation/ansible/deploy-ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing Ansible..."
-sudo apt-get install ansible sshpass
+sudo apt-get install ansible ansible-lint sshpass
 
 echo "Installing some Galaxy packages..."
 ansible-galaxy role install simoncaron.pve_permissions
