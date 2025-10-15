7b1933e6a257ddde436d6ea5c3df34bc2498b649
Zoltan Kutasi <z.kutasi@gmail.com>
2025-10-15 10:50:09 +0200

    Remove Compose V1 support from Ansible role

--- automation/ansible/deploy-ansible.sh
+++ automation/ansible/deploy-ansible.sh
@@ -5,3 +5,4 @@ sudo apt-get install ansible ansible-lint sshpass
 
 echo "Installing some Galaxy packages..."
 ansible-galaxy role install simoncaron.pve_permissions
+ansible-galaxy collection install community.docker --force
