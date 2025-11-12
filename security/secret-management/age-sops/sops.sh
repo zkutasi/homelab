507af13088d06d193f322242ae7287281ee7520f
Zoltan Kutasi <z.kutasi@gmail.com>
2025-11-12 10:28:34 +0100

    Add a reencrypt-all flag to sops.sh

--- security/secret-management/age-sops/sops.sh
+++ security/secret-management/age-sops/sops.sh
@@ -6,11 +6,12 @@ MODE=decrypt-all
 FILE_TYPE="binary"
 
 function usage() {
-    echo "Usage: $0 [--decrypt-all] [--encrypt <file>]"
+    echo "Usage: $0 [--decrypt-all] [--encrypt <file>] [--reencrypt-all]"
     echo ""
     echo "Options:"
     echo "  --decrypt-all         Decrypt all files ending with '_encrypted' in the repository (default)"
     echo "  --encrypt <file>      Encrypt the specified file and save it as '<file>_encrypted'"
+    echo "  --reencrypt-all       Re-encrypt all encrypted files from their non-encrypted counterparts"
     echo ""
     echo "Environment Variables:"
     echo "  SOPS_AGE_KEY_FILE     Path to the file containing the age public key (required)"
@@ -25,6 +26,9 @@ while [ $# -ge 1 ]; do
         MODE=encrypt
         shift
         ENCRYPT_FILE=$1
+        ;;
+    --reencrypt-all)
+        MODE=reencrypt-all
         ;;
     *)
       echo "ERROR: unknown parameter \"$1\""
@@ -69,6 +73,33 @@ elif [ "${MODE}" == "decrypt-all" ]; then
                 --age ${AGE_KEY} \
                 --output "${decrypted_filename}" \
                 "${line}"
+        fi
+    done < <(find $(git rev-parse --show-toplevel) -type f -name "*_encrypted")
+elif [ "${MODE}" == "reencrypt-all" ]; then
+    while read -r line; do
+        decrypted_filename=${line%_encrypted}
+        if [ -f "${decrypted_filename}" ]; then
+            echo "Checking for changes in ${decrypted_filename} to re-encrypt..."
+            diff -u <(sops --decrypt \
+                --input-type ${FILE_TYPE} \
+                --output-type ${FILE_TYPE} \
+                --age ${AGE_KEY} \
+                "${line}") \
+                "${decrypted_filename}" >/dev/null
+            res=$?
+            if [ $res -eq 0 ]; then
+                echo "No differences found. Skipping re-encryption for ${line}."
+            elif [ $res -eq 1 ]; then
+                echo "Differences found. Re-encrypting ${decrypted_filename} to ${line}..."
+                sops --encrypt \
+                    --input-type ${FILE_TYPE} \
+                    --output-type ${FILE_TYPE} \
+                    --age ${AGE_KEY} \
+                    --output "${line}" \
+                    "${decrypted_filename}"
+            fi
+        else
+            echo "WARNING: Unencrypted file ${decrypted_filename} not found. Skipping re-encryption for ${line}."
         fi
     done < <(find $(git rev-parse --show-toplevel) -type f -name "*_encrypted")
 else
