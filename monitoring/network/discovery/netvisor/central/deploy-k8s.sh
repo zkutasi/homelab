1fd53731beeab36575af1b23818f54138bb9d1ad
Zoltan Kutasi <z.kutasi@gmail.com>
2025-11-19 16:23:32 +0100

    Modify NetVisor install to have its CNPG from TrueCharts

--- monitoring/network/discovery/netvisor/central/deploy-k8s.sh
+++ monitoring/network/discovery/netvisor/central/deploy-k8s.sh
@@ -3,7 +3,6 @@
 APP=netvisor
 NS=netvisor
 RELEASE_NAME=netvisor
-REPO_URL_CNPG=https://cloudnative-pg.github.io/charts
 
 EXTRA_PARAMS=
 
@@ -16,33 +15,15 @@ while [ $# -ge 1 ]; do
   shift
 done
 
-DB_SECRET_NAME="${APP}-db-password-secret"
-DB_USERNAME="netvisor"
-DB_HOST="postgresql-cluster-rw"
-DB_DATABASE="netvisor"
-if ! $(kubectl -n $NS get secret ${DB_SECRET_NAME} &> /dev/null); then
-  echo "Generating random database password..."
-  DB_PASSWORD="$(dd if=/dev/urandom bs=1 count=12 status=none | base64 | tr -dc 'a-zA-Z0-9' | head -c 12)"
-
-  kubectl create namespace $NS
-  kubectl create secret generic ${DB_SECRET_NAME} \
-      --from-literal=username="${DB_USERNAME}" \
-      --from-literal=password="${DB_PASSWORD}" \
-      --type=kubernetes.io/basic-auth \
-      --namespace=$NS
+if [ ! -f app-values-private.yaml ]; then
+    touch app-values-private.yaml
 fi
-DB_PASSWORD=$(kubectl get secret -n $NS ${DB_SECRET_NAME} -o jsonpath='{.data.password}' | base64 -d)
-DB_CONNECTION_STRING="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_DATABASE}"
-yq -i ".secret.db-credentials.data.database-url=\"${DB_CONNECTION_STRING}\"" app-values-private.yaml
 
-echo "Deploying the PostgreSQL database..."
-$(git rev-parse --show-toplevel)/common-deploy-helm.sh \
-    --chart-name cnpg/cluster \
-    --release-name postgresql \
-    --namespace $NS \
-    --repo-url ${REPO_URL_CNPG} \
-    --app postgresql-cluster \
-    ${EXTRA_PARAMS}
+DB_PASSWORD=$(yq '.cnpg.main.password' app-values-private.yaml)
+if [[ "${DB_PASSWORD}" == "null" ]]; then
+    DB_PASSWORD="$(dd if=/dev/urandom bs=1 count=12 status=none | base64 | tr -dc 'a-zA-Z0-9' | head -c 12)"
+    yq -i ".cnpg.main.password=\"${DB_PASSWORD}\"" app-values-private.yaml
+fi
 
 $(git rev-parse --show-toplevel)/common-deploy-helm.sh \
     --chart-name ${PWD}/chart \
