84f9883613846ccd2a5dc64b6a5c1db01fc5d1d7
Zoltan Kutasi <z.kutasi@gmail.com>
2026-01-10 11:26:57 +0100

    Add missing DB Password handling for DockHand

--- container-mgmt/dockhand/central/deploy-k8s.sh
+++ container-mgmt/dockhand/central/deploy-k8s.sh
@@ -15,6 +15,16 @@ while [ $# -ge 1 ]; do
   shift
 done
 
+if [ ! -f app-values-private.yaml ]; then
+    touch app-values-private.yaml
+fi
+
+DB_PASSWORD=$(yq '.cnpg.main.password' app-values-private.yaml)
+if [[ "${DB_PASSWORD}" == "null" ]]; then
+    DB_PASSWORD=$(dd if=/dev/urandom bs=1 count=12 status=none | base64 | tr -dc 'a-zA-Z0-9' | head -c 12)
+    yq -i ".cnpg.main.password=\"${DB_PASSWORD}\"" app-values-private.yaml
+fi
+
 $(git rev-parse --show-toplevel)/common-deploy-helm.sh \
     --chart-name ${PWD}/chart \
     --namespace $NS \
