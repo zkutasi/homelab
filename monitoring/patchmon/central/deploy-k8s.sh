d8bb9522de0228ef50b92210faa0f46f80cb85f5
Zoltan Kutasi <z.kutasi@gmail.com>
2026-01-29 15:59:42 +0100

    Add Patchmon with working solution

--- monitoring/patchmon/central/deploy-k8s.sh
+++ monitoring/patchmon/central/deploy-k8s.sh
@@ -23,12 +23,20 @@ REDIS_PASSWORD=$(yq '.redis.password' app-values-private.yaml)
 if [[ "${REDIS_PASSWORD}" == "null" ]]; then
     REDIS_PASSWORD="$(dd if=/dev/urandom bs=1 count=12 status=none | base64 | tr -dc 'a-zA-Z0-9' | head -c 12)"
     yq -i ".redis.password=\"${REDIS_PASSWORD}\"" app-values-private.yaml
+    yq -i ".workload.main.podSpec.containers.backend.env.REDIS_PASSWORD=\"${REDIS_PASSWORD}\"" app-values-private.yaml
 fi
 
 DB_PASSWORD=$(yq '.cnpg.main.password' app-values-private.yaml)
 if [[ "${DB_PASSWORD}" == "null" ]]; then
     DB_PASSWORD="$(dd if=/dev/urandom bs=1 count=12 status=none | base64 | tr -dc 'a-zA-Z0-9' | head -c 12)"
     yq -i ".cnpg.main.password=\"${DB_PASSWORD}\"" app-values-private.yaml
+    yq -i ".workload.main.podSpec.containers.backend.env.DB_PASSWORD=\"${DB_PASSWORD}\"" app-values-private.yaml
+fi
+
+JWT_SECRET=$(yq '.workload.main.podSpec.containers.backend.env.JWT_SECRET' app-values-private.yaml)
+if [[ "${JWT_SECRET}" == "null" ]]; then
+    JWT_SECRET="$(openssl rand -hex 64)"
+    yq -i ".workload.main.podSpec.containers.backend.env.JWT_SECRET=\"${JWT_SECRET}\"" app-values-private.yaml
 fi
 
 $(git rev-parse --show-toplevel)/common-deploy-helm.sh \
