0f31bfff38eac40ca5740112f1b4d6b9d4b2410c
Zoltan Kutasi <z.kutasi@gmail.com>
2026-06-16 18:12:31 +0200

    Simplify binary handling and make similar to images for Renovate

--- automation/gitops/renovate/dry-run.sh
+++ automation/gitops/renovate/dry-run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+[ -z "${RENOVATE_TOKEN}" ] && echo "RENOVATE_TOKEN is not set" && exit 1
+
 docker run --rm -it \
     --volume "$(git rev-parse --show-toplevel):/repo" \
     --workdir /repo \
     --env LOG_LEVEL=debug \
+    --env RENOVATE_TOKEN=${RENOVATE_TOKEN}$ \
+    --env RENOVATE_GITHUB_COM_TOKEN=${RENOVATE_TOKEN} \
     renovate/renovate:43.220 \
     renovate \
     --platform=local \
