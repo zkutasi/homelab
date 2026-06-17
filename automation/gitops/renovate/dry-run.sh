20f40f3367a4a9fdcc06eb3d21786cd8864664b5
Zoltan Kutasi <z.kutasi@gmail.com>
2026-06-17 11:34:44 +0200

    Improve renovate dry-run.sh

--- automation/gitops/renovate/dry-run.sh
+++ automation/gitops/renovate/dry-run.sh
@@ -2,13 +2,17 @@
 
 [ -z "${RENOVATE_TOKEN}" ] && echo "RENOVATE_TOKEN is not set" && exit 1
 
+RENOVATE_DIR=$(git rev-parse --show-toplevel)/automation/gitops/renovate
+RENOVATE_IMAGE_REPO=$(yq '.image.registry' ${RENOVATE_DIR}/renovate-values.yaml)
+RENOVATE_IMAGE_TAG=$(yq '.image.tag' ${RENOVATE_DIR}/renovate-values.yaml)
+
 docker run --rm -it \
     --volume "$(git rev-parse --show-toplevel):/repo" \
     --workdir /repo \
     --env LOG_LEVEL=debug \
-    --env RENOVATE_TOKEN=${RENOVATE_TOKEN}$ \
+    --env RENOVATE_TOKEN=${RENOVATE_TOKEN} \
     --env RENOVATE_GITHUB_COM_TOKEN=${RENOVATE_TOKEN} \
-    renovate/renovate:43.220 \
+    ${RENOVATE_IMAGE_REPO}:${RENOVATE_IMAGE_TAG} \
     renovate \
     --platform=local \
     --dry-run=full \
