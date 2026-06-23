aeed2e9be64d5d09460c24d9956b2da0a93e7a84
Zoltan Kutasi <z.kutasi@gmail.com>
2026-06-23 12:27:27 +0200

    Improve sops.sh

--- security/secret-mgmt/age-sops/sops.sh
+++ security/secret-mgmt/age-sops/sops.sh
@@ -6,12 +6,13 @@ MODE=decrypt-all
 FILE_TYPE="binary"
 
 function usage() {
-    echo "Usage: $0 [--decrypt-all] [--encrypt <file>] [--reencrypt-all]"
+    echo "Usage: $0 [--encrypt <file>] [--encrypt-all] [--decrypt <file>] [--decrypt-all] [--reencrypt-all]"
     echo ""
     echo "Options:"
-    echo "  --decrypt-all         Decrypt all files ending with '_encrypted' in the repository (default)"
     echo "  --encrypt <file>      Encrypt the specified file and save it as '<file>_encrypted'"
-    echo "  --encrypt-all-missing <filename> Find all files named <filename>, and if not empty and no encrypted version exists, encrypt them."
+    echo "  --encrypt-all         Encrypt all files having 'private' in their name and save them as '<file>_encrypted'"
+    echo "  --decrypt <file>      Decrypt the specified file (which should end with '_encrypted') and save it as '<file>'"
+    echo "  --decrypt-all         Decrypt all files ending with '_encrypted' in the repository (default)"
     echo "  --reencrypt-all       Re-encrypt all encrypted files from their non-encrypted counterparts"
     echo ""
     echo "Environment Variables:"
@@ -20,6 +21,11 @@ function usage() {
 
 while [ $# -ge 1 ]; do
   case "$1" in
+    --decrypt)
+        MODE=decrypt
+        shift
+        DECRYPT_FILE=$1
+        ;;
     --decrypt-all)
         MODE=decrypt-all
         ;;
@@ -28,10 +34,8 @@ while [ $# -ge 1 ]; do
         shift
         ENCRYPT_FILE=$1
         ;;
-    --encrypt-all-missing)
-        MODE=encrypt-all-missing
-        shift
-        MISSING_FILE_NAME=$1
+    --encrypt-all)
+        MODE=encrypt-all
         ;;
     --reencrypt-all)
         MODE=reencrypt-all
@@ -53,6 +57,18 @@ if [ "${MODE}" == "encrypt" ]; then
         --age ${AGE_KEY} \
         --output "${ENCRYPT_FILE}_encrypted" \
         "${ENCRYPT_FILE}"
+elif [ "${MODE}" == "decrypt" ]; then
+    echo "Decrypting file: ${DECRYPT_FILE}..."
+    if [[ ! "${DECRYPT_FILE}" == *_encrypted ]]; then
+        echo "ERROR: file to decrypt must end with '_encrypted'"
+        exit 1
+    fi
+    sops --decrypt \
+        --input-type ${FILE_TYPE} \
+        --output-type ${FILE_TYPE} \
+        --age ${AGE_KEY} \
+        --output "${DECRYPT_FILE%_encrypted}" \
+        "${DECRYPT_FILE}"
 elif [ "${MODE}" == "decrypt-all" ]; then
     while read -r line; do
         echo "Decrypting file: ${line}..."
@@ -108,12 +124,20 @@ elif [ "${MODE}" == "reencrypt-all" ]; then
             echo "WARNING: Unencrypted file ${decrypted_filename} not found. Skipping re-encryption for ${line}."
         fi
     done < <(find $(git rev-parse --show-toplevel) -type f -name "*_encrypted")
-elif [ "${MODE}" == "encrypt-all-missing" ]; then
+elif [ "${MODE}" == "encrypt-all" ]; then
     while read -r line; do
-        if [ -s "${line}" ]; then
-            encrypted_filename="${line}_encrypted"
-            if [ ! -f "${encrypted_filename}" ]; then
-                echo "Encrypting missing encrypted file for: ${line}..."
+        echo "Encrypting file: ${line}..."
+        encrypted_filename=${line}_encrypted
+        if [ -f "${encrypted_filename}" ]; then
+            diff -u <(sops --encrypt \
+                --input-type ${FILE_TYPE} \
+                --output-type ${FILE_TYPE} \
+                --age ${AGE_KEY} \
+                "${line}") \
+                "${decrypted_filename}"
+            res=$?
+            if [ $res -eq 1 ]; then
+                echo "Differences found between encrypted content and existing file. Re-encrypting..."
                 sops --encrypt \
                     --input-type ${FILE_TYPE} \
                     --output-type ${FILE_TYPE} \
@@ -121,8 +145,15 @@ elif [ "${MODE}" == "encrypt-all-missing" ]; then
                     --output "${encrypted_filename}" \
                     "${line}"
             fi
+        else
+            sops --encrypt \
+                --input-type ${FILE_TYPE} \
+                --output-type ${FILE_TYPE} \
+                --age ${AGE_KEY} \
+                --output "${encrypted_filename}" \
+                "${line}"
         fi
-    done < <(find $(git rev-parse --show-toplevel) -type f -name "${MISSING_FILE_NAME}")
+    done < <(find $(git rev-parse --show-toplevel) -type f -name "*private*" ! -name "*_encrypted" ! -name "*.j2")
 else
     echo "ERROR: unknown mode \"${MODE}\""
     exit 1
