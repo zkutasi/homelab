e145470e8b2cb64c986e1fd3d96c422a523b39e2
Zoltan Kutasi <z.kutasi@gmail.com>
2026-06-23 17:54:17 +0200

    Remove obsolete random generation from deploy scripts

--- monitoring/uptime/checkmate/central/deploy-k8s.sh
+++ monitoring/uptime/checkmate/central/deploy-k8s.sh
@@ -15,12 +15,6 @@ while [ $# -ge 1 ]; do
   shift
 done
 
-JWT_SECRET=$(yq '.workload.main.podSpec.containers.main.env.JWT_SECRET' app-values-private.yaml)
-if [[ "${JWT_SECRET}" == "null" ]]; then
-  JWT_SECRET=$(dd if=/dev/urandom bs=1 count=12 status=none | base64 | tr -dc 'a-zA-Z0-9' | head -c 12)
-  yq -i ".workload.main.podSpec.containers.main.env.JWT_SECRET=\"${JWT_SECRET}\"" app-values-private.yaml
-fi
-
 kubectl create secret generic ca-certificates \
     --namespace $NS \
     --from-file=homelab-ca.pem=config/static/ca.crt \
