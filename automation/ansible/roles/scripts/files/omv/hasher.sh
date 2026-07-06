2e5e65b7724dfb20deb01ef55d2548a2d19c27e8
Zoltan Kutasi <z.kutasi@gmail.com>
2026-07-06 21:01:23 +0200

    Execute the hasher script after every completed download automatically

--- automation/ansible/roles/scripts/files/omv/hasher.sh
+++ automation/ansible/roles/scripts/files/omv/hasher.sh
@@ -5,33 +5,46 @@
 #    - A sane and simple naming convention for the drives:
 #      > Labelled for example hddXX, where XX goes 01,02,03,...
 #      > Mounted by label with a sane naming convention
-#    - Setting up the DRIVE_DIR, DRIVES and LABELS variables properly, so you end up with a list of your drives and labels
+#    - Setting up the drive_dir, drives variables properly, so you end up with a list of your required drives
 #      Of course if you have mounts and labels all around the place, give them manually
 #    - A sane directory structure, same on every HDD (Movies, Animation, Anime, Docu, etc...)
-# Arguments: NONE
+# Arguments: If a specific subfolder needs to be traversed, that folder, otherwise we traverse the whole host
 
-DRIVE_DIR="/srv/dev-disk-by-label-hdd0"
-DRIVES=$(seq -w 1 7 | sed "s,^,${DRIVE_DIR},")
-
-for drive in ${DRIVES}; do
-  echo "Handling drive ${drive}..."
-  for type in Animation Anime Docu Movies AnimeSeries DocuSeries TVSeries; do
-    i=1
-    num=$(find ${drive}/${type} -type f -name '*.mkv' | wc -l)
-    while IFS= read -r file; do
-      if [ -n "$file" ]; then
-        if [ ! -s "${file}.sha" ]; then
-          filename=$(basename "${file}")
-          dirname=$(dirname "${file}")
-          echo "[${i}/${num}] Creating SHA hash for ${file}..."
-          pushd "${dirname}" 2>&1 > /dev/null
-          sha1sum "${filename}" > "${filename}.sha"
-          popd 2>&1 > /dev/null
-        else
-          echo "[${i}/${num}] ${file} already has a SHA hash"
-        fi
+function hash_all_mkv_files()
+{
+  path="$1"
+  i=1
+  num=$(find "${path}" -type f -name '*.mkv' | wc -l)
+  while IFS= read -r file; do
+    if [ -n "${file}" ]; then
+      if [ ! -s "${file}.sha" ]; then
+        filename=$(basename "${file}")
+        dirname=$(dirname "${file}")
+        echo "[${i}/${num}] Creating SHA hash for ${file}..."
+        pushd "${dirname}" 2>&1 > /dev/null
+        sha1sum "${filename}" > "${filename}.sha"
+        popd 2>&1 > /dev/null
+      else
+        echo "[${i}/${num}] ${file} already has a SHA hash"
       fi
-      ((i++))
-    done <<< $(find ${drive}/${type} -type f -name '*.mkv')
+    fi
+    ((i++))
+  done < <(find "${path}" -type f -name '*.mkv')
+}
+
+if [ $# -gt 1 ]; then
+  echo "ERROR: Too many arguments!"
+elif [ $# -eq 1 ]; then
+  hash_all_mkv_files "$1"
+else
+  echo "Traversing all the disks..."
+  drive_dir="/srv/dev-disk-by-label-hdd0"
+  drives=$(seq -w 1 7 | sed "s,^,${drive_dir},")
+
+  for drive in ${drives}; do
+    echo "Handling drive ${drive}..."
+    for type in Animation Anime Docu Movies AnimeSeries DocuSeries TVSeries; do
+      hash_all_mkv_files "${drive}/${type}"
+    done
   done
-done
+fi
