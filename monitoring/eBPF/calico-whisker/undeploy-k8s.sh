661180f330fb7b8cfe271987bb3b97b2fb891ce5
Zoltan Kutasi <z.kutasi@gmail.com>
2026-09-22 18:22:25 +0200

    Fix Whisker cleanup

--- monitoring/eBPF/calico-whisker/undeploy-k8s.sh
+++ monitoring/eBPF/calico-whisker/undeploy-k8s.sh
@@ -6,9 +6,12 @@ kubectl -n $NS delete serviceaccount goldmane
 kubectl -n $NS delete service goldmane
 kubectl -n $NS delete configmap goldmane
 kubectl -n $NS delete deployment goldmane
+kubectl -n $NS delete secret goldmane-internal-ca-secret
+kubectl -n $NS delete secret goldmane-tls
 
 kubectl -n $NS delete serviceaccount whisker
 kubectl -n $NS delete service whisker
 kubectl -n $NS delete deployment whisker
 kubectl -n $NS delete certificate whisker-cert
 kubectl -n $NS delete httpproxy whisker-httpproxy
+kubectl -n $NS delete secret whisker-backend-tls
